158e1b512ed11280cb460a22155ed49d37a9fbca
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-07-31 14:03:49 +0200

    Add comments

--- lib/unit_float.py
+++ lib/unit_float.py
@@ -1,6 +1,11 @@
+from functools import wraps
+
 
 class UnitFloat(float):
+    """A typed float, i.e. a float with a unit."""
+
     unit: str
+    """The unit of this float."""
 
     def __new__(cls, value: float, unit: str):
         return float.__new__(cls, value)
@@ -12,10 +17,20 @@ class UnitFloat(float):
     def __str__(self):
         return f'{super.__str__(self)} {self.unit}'
 
+    def __repr__(self):
+        return f'UnitFloat({repr(float(self))}, {self.unit})'
+
 
 def return_unit(unit: str):
+    """Changes the return type of the decorated function from float to a UnitFloat.
+
+    :param unit: The unit of the returned UnitFloat.
+    """
     def decorator(function):
+        # @wraps copies the function name, docstring and argument list from the decorated function to the wrapper.
+        @wraps(function)
         def wrapper(*args, **kwargs):
+            # Call function and wrap return value in UnitFloat
             return_value = function(*args, **kwargs)
             return UnitFloat(return_value, unit)
         return wrapper
