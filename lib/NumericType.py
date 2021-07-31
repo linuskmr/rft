0cd6b0e5f9eb44bb5e5689acd23b6128c0147d4c
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-07-31 12:05:26 +0200

    Add NumericType

---
+++ lib/NumericType.py
@@ -0,0 +1,29 @@
+
+class MyNumber(float):
+    unit: str
+
+    def __new__(cls, value: float, unit: str):
+        return float.__new__(cls, value)
+
+    def __init__(self, value: float, unit: str):
+        float.__init__(value)
+        self.unit = unit
+
+    def __str__(self):
+        return f'{super.__str__(self)} m/s'
+
+
+def str_return(function):
+    def inner(*args, **kwargs):
+        return_value = function(*args, **kwargs)
+        return MyNumber(return_value, 'm/s')
+
+    return inner
+
+
+@str_return
+def speed(distance: float, duration: float) -> float:
+    return distance / duration
+
+
+# print(speed(100, 10) + NumericType(3, 'm/s'))
