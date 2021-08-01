d5fb3acd8fd7a01064132eba1802d66020e12789
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-08-01 12:43:48 +0200

    Add Perizentrumsgeschwindigkeit

--- fluchthyperbel.py
+++ fluchthyperbel.py
@@ -1,7 +1,6 @@
-# ! Imports nicht optimieren bzw. welche rauslöschen, um in main() via eval() darauf Zugriff zu haben !
 import json
 from typing import Dict
-from lib import ellipse, kreis, allgemein, konstanten, hyperbel
+from lib import kreis, hyperbel
 from lib.planet import *
 from lib.unit_float import UnitFloat
 
@@ -11,31 +10,33 @@ def fluchthyperbel(planet: Planet, vinf: float, hp: float) -> Dict[str, float]:
     hp = UnitFloat(hp, 'km')
 
     print('Fluchthyperbel 🚀')
-    print(f'Planet {planet}')
+    print(f'{planet}')
     print(f'Geschwindigkeit im Unendlichen {vinf=}')
+    print(f'Höhe über Perizentrum {hp=}')
     print()
 
+    rp = hp + planet.R
+    print(f'Radius Perizentrum {rp=}')
     a = hyperbel.grosse_halbachse_planet_vinf(planet=planet, vinf=vinf)
     print(f'Große Halbachse {a=}')
-
-    rp = hp + planet.R
-    print(f'Radius Perizentrum: {rp=}')
-
     epsilon = hyperbel.numerische_exzentrizitaet(a=a, rp=rp)
     print(f'Numerische Exzentrizität {epsilon=}')
-
     p = hyperbel.bahnparameter_p(a=a, epsilon=epsilon)
     print(f'Bahnparameter {p=}')
-
     grosses_epsilon = hyperbel.umlenkwinkel(epsilon=epsilon)
     print(f'Umlenkwinkel {grosses_epsilon}')
-
     psi_inf = hyperbel.unendlichkeitsanomalie(epsilon=epsilon)
     print(f'Unendlichkeitsanomalie {psi_inf=}')
+    vk = kreis.geschwindigkeit(planet=planet, r=rp)
+    print(f'Bereits vorhandene Kreisbahngeschwindigkeit bei Perizentrum mit Radius {rp}: {vk=}')
+    vp = hyperbel.perizentrum_geschwindigkeit(vk=vk, vinf=vinf)
+    print(f'Perizentrumsgeschwindigkeit {vp=}')
+    v_total = vp - vk
+    print(f'Benötigter Gesamtschubimpuls {v_total}')
 
     return {
         'vinf': vinf, 'hp': hp, 'a': a, 'rp': rp, 'epsilon': epsilon, 'p': p, 'grosses_epsilon': grosses_epsilon,
-        'psi_inf': psi_inf
+        'psi_inf': psi_inf, 'vk': vk, 'vp': vp, 'v_total': v_total
     }
 
 
