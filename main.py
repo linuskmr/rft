42e4c5279600894b939f8c2c7c4be3cfe6b50304
Max <m.giller.dev@gmail.com>
2021-08-04 18:06:51 +0200

    (Inacurate) example of auto-solve

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from lib.ellipse import Ellipse
 from lib.planet import *
-from lib.allgemein import *
-from lib import ellipse, kreis, hyperbel, parabel
+from lib.unit_decimal import UnitDecimal
 
+el = Ellipse(a=UnitDecimal('463_971_810', 'km'),
+             e=UnitDecimal('314_372_451', 'km'),
+             zentralgestirn=SONNE)
+
+print(el.p)
