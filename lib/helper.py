8d1fa17cac4e953dc892b0cdce6872b64f579d23
Max <m.giller.dev@gmail.com>
2021-08-04 20:23:53 +0200

    Added time converter and param_funcs merger

--- lib/helper.py
+++ lib/helper.py
@@ -1,7 +1,7 @@
 from lib.unit_decimal import return_unit
 from decimal import Decimal
 import math
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 @return_unit("°")
@@ -55,3 +55,48 @@ def gleicher_tag(datum1: datetime, datum2: datetime) -> bool:
         bool: True, wenn Jahr, Monat und Tag übereinstimmen.
     """
     return datum1.year == datum2.year and datum1.month == datum2.month and datum1.day == datum2.day
+
+
+def jahre_zu_timedelta(jahre: Decimal) -> timedelta:
+    """Konvertiert Jahre zu Tage mit einer durchschnittlichen Anzahl an 356.2422 Tagen pro Jahr.
+
+    Args:
+        jahre (Decimal): Jahre in Jahren.
+
+    Returns:
+        timedelta: Timedelta bestimmt durch Tage.
+    """
+    return timedelta(days=float(Decimal('365.2422') * jahre))
+
+
+@return_unit("a")
+def timedelta_zu_jahre(td: timedelta) -> Decimal:
+    """Konvertiert ein timedelta Objekt basierend auf Tagen zu Jahren.
+
+    Args:
+        td (timedelta): Timedelta.
+
+    Returns:
+        Decimal: Jahre in Jahren.
+    """
+    return td.days / Decimal('365.2422')
+
+
+def merge_param_funcs(base_funcs: dict, additional_funcs: dict) -> dict:
+    """Merges two param funcs dictionaries.
+
+    Args:
+        base_funcs ([type]): A param func dict, which will be used as the resulting dictionary.
+        additional_funcs ([type]): A param func dict.
+
+    Returns:
+        dict: The merged dict, the modified base_funcs dict.
+    """
+
+    for key in additional_funcs.keys():
+        if key not in base_funcs.keys():
+            base_funcs[key] = []
+
+        base_funcs[key].extend(additional_funcs[key])
+
+    return base_funcs
