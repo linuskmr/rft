3f815d506e2e9881d024bbb1ef5bcd63ac3bdc0e
Max <m.giller.dev@gmail.com>
2021-08-07 19:11:14 +0200

    Added task from old exam

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from bahnen.transfer_ellipse import TransferEllipse
+from bahnen.ellipse import Ellipse
 from lib.planet import *
 from lib.unit_decimal import UnitDecimal
+from datetime import timedelta
+
+ellipse: Ellipse = Ellipse(zentralgestirn=SONNE,
+                           ra=UnitDecimal('147_134_000', 'km'),
+                           umlaufzeit=timedelta(days=187))
+
+print("Ellipse from WS 20/21 A.6 done")
+
 
 transfer: TransferEllipse = TransferEllipse(zentralgestirn=SONNE,
                            start_planet=ERDE,
