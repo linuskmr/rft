31c1ed07e0f452174451ff591b373c65ba495485
Max <m.giller.dev@gmail.com>
2021-08-08 19:03:02 +0200

    Created separate example.py

--- main.py
+++ main.py
@@ -5,29 +5,4 @@ from lib.planet import *
 from lib.unit_decimal import UnitDecimal
 from datetime import timedelta
 
-ellipse: Ellipse = Ellipse(zentralgestirn=SONNE,
-                           ra=UnitDecimal('147_134_000', 'km'),
-                           umlaufzeit=timedelta(days=187))
-
-print("Ellipse from WS 20/21 A.6 done")
-
-
-hohmann: HohmannTransfer = HohmannTransfer(
-    zentralgestirn=SONNE,
-    ziel_planet=PLUTO,
-    start_planet=ERDE
-)
-
-print(hohmann.transfer_dauer)
-
-
-transfer: TransferEllipse = TransferEllipse(zentralgestirn=SONNE,
-                           start_planet=ERDE,
-                           ziel_planet=MARS,
-                           epsilon=UnitDecimal('0.5237',''),
-                           p=MARS.a)
-
-for i in range(10):
-    print(transfer.startzeitpunkt_nach_index(i))
-
-print("Done")
+# Let the journey begin 🚀
