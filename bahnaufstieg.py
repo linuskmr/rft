cd9a29af5f084c04d762aaaf44f520d52ced5d85
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-08-08 18:53:03 +0200

    Add gewinn_erdrotation()

--- bahnaufstieg.py
+++ bahnaufstieg.py
@@ -1,11 +1,39 @@
+import math
 from dataclasses import dataclass
-from lib.unit_decimal import UnitDecimal
+from decimal import Decimal
+
+from lib import konstanten
+from lib.helper import grad_zu_rad
+from lib.planet import ERDE
+from lib.unit_decimal import UnitDecimal, return_unit
 
 
 @dataclass(frozen=True)
 class Bahnaufstieg:
     v_total: UnitDecimal
     """Insgesamt benötigter Schubimpuls in km/s."""
+
+
+@return_unit('km/s')
+def gewinn_erdrotation(beta: Decimal) -> Decimal:
+    """
+    Berechnet den Gewinn durch die Erdrotation auf dem Breitengrad beta.
+
+    >>> from decimal import getcontext
+    >>> getcontext().prec = 3
+    >>> gewinn_erdrotation(Decimal(0))
+    0.465 km/s
+    >>> gewinn_erdrotation(Decimal(90))
+    0.000 km/s
+
+    Args:
+        beta: Breitengrad, auf dem gestartet wird, in Grad.
+
+    Returns:
+        Decimal: Das gewonnene delta_v durch die Erdrotation.
+    """
+    r = ERDE.R * Decimal(math.cos(grad_zu_rad(beta)))
+    return (2 * Decimal(math.pi) * r) / Decimal(konstanten.SIDERISCHER_TAG.total_seconds())
 
 
 def bahnaufstieg() -> Bahnaufstieg:
