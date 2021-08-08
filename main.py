48bb7c674fdb3c6359ac533e84ff9834bd5732fc
Max <m.giller.dev@gmail.com>
2021-08-08 13:27:33 +0200

    Added example calculations for Hohmann Transfer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bahnen.transfer_ellipse import TransferEllipse
 from bahnen.ellipse import Ellipse
+from bahnen.hohmann import HohmannTransfer
 from lib.planet import *
 from lib.unit_decimal import UnitDecimal
 from datetime import timedelta
@@ -9,6 +10,14 @@ ellipse: Ellipse = Ellipse(zentralgestirn=SONNE,
                            umlaufzeit=timedelta(days=187))
 
 print("Ellipse from WS 20/21 A.6 done")
+
+
+hohmann: HohmannTransfer = HohmannTransfer(
+    ziel_planet=PLUTO,
+    start_planet=ERDE
+)
+
+print(hohmann.transfer_dauer)
 
 
 transfer: TransferEllipse = TransferEllipse(zentralgestirn=SONNE,
